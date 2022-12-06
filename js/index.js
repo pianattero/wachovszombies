74cb4cf0ed2cefa5c3155501a7c053ac20ed87b7
pianattero <mariapianattero@gmail.com>
2022-12-06 13:48:38 +0100

    Added instruction page

--- js/index.js
+++ js/index.js
@@ -4,11 +4,14 @@ canvasDiv = document.querySelector('.canvas-div');
 startBtn = document.querySelector('.menu-btns .start-btn');
 howToPlayBtn = document.querySelector('.menu-btns .how-to-play-btn');
 nextBtn = document.querySelector('.next-btn');
+nextBtn2 = document.querySelector('.next-btn2');
 previousBtn = document.querySelector('.previous-btn');
+previousBtn2 = document.querySelector('.previous-btn2');
 homeBtn = document.querySelector('.home-btn');
 homeDiv = document.querySelector('.home-page');
 howToPlayDiv1 = document.querySelector('.how-to-play-page1');
 howToPlayDiv2 = document.querySelector('.how-to-play-page2');
+howToPlayDiv3 = document.querySelector('.how-to-play-page3');
 
 
 startBtn.addEventListener('click', () => {
@@ -36,6 +39,13 @@ nextBtn.addEventListener('click', () => {
 	howToPlayDiv2.classList.add('flex');
 });
 
+nextBtn2.addEventListener('click', () => {
+	howToPlayDiv2.classList.remove('flex');
+	howToPlayDiv2.classList.add('hidden');
+	howToPlayDiv3.classList.remove('hidden');
+	howToPlayDiv3.classList.add('flex');
+});
+
 previousBtn.addEventListener('click', () => {
 	howToPlayDiv2.classList.remove('flex');
 	howToPlayDiv2.classList.add('hidden');
@@ -43,11 +53,18 @@ previousBtn.addEventListener('click', () => {
 	howToPlayDiv1.classList.add('flex');
 });
 
+previousBtn2.addEventListener('click', () => {
+	howToPlayDiv3.classList.remove('flex');
+	howToPlayDiv3.classList.add('hidden');
+	howToPlayDiv2.classList.remove('hidden');
+	howToPlayDiv2.classList.add('flex');
+});
+
 homeBtn.addEventListener('click', () => {
 	homeDiv.classList.remove('hidden');
 	homeDiv.classList.add('flex');
-	howToPlayDiv2.classList.remove('flex');
-	howToPlayDiv2.classList.add('hidden');
+	howToPlayDiv3.classList.remove('flex');
+	howToPlayDiv3.classList.add('hidden');
 });
 
 document.addEventListener('keydown', function(event) {
